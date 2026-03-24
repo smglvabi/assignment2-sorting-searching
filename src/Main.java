a4708b366ce501f0cfad69f692ed484e3fba2fa2
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 23:13:46 +0500

    task9

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    static boolean onlyDigits(String s, int index) {
+    static int countUpper(String s, int index) {
+
         if (index == s.length()) {
-            return true;
+            return 0;
         }
 
-        if (!Character.isDigit(s.charAt(index))) {
-            return false;
+        char c = s.charAt(index);
+
+        if (Character.isUpperCase(c)) {
+            return 1 + countUpper(s, index + 1);
         }
 
-        return onlyDigits(s, index + 1);
+        return countUpper(s, index + 1);
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         String s = sc.nextLine();
 
-        if (onlyDigits(s, 0)) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        System.out.println(countUpper(s, 0));
+
     }
 }
