5db4807b0af06ad3a56edee3c6200d92e20a2a15
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 23:06:50 +0500

    task8

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void readArray(int[] arr, int index, Scanner sc) {
-
-        if (index == arr.length) {
-            return;
+    static boolean onlyDigits(String s, int index) {
+        if (index == s.length()) {
+            return true;
         }
 
-        arr[index] = sc.nextInt();
-
-        readArray(arr, index + 1, sc);
-    }
-
-    static void reverse(int[] arr, int index) {
-
-        if (index == arr.length) {
-            return;
+        if (!Character.isDigit(s.charAt(index))) {
+            return false;
         }
 
-        reverse(arr, index + 1);
-
-        System.out.print(arr[index] + " ");
+        return onlyDigits(s, index + 1);
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        String s = sc.nextLine();
 
-        int[] arr = new int[n];
-
-        readArray(arr, 0, sc);
-
-        reverse(arr, 0);
-
+        if (onlyDigits(s, 0)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
